eb3e7b7aa64c73aa0f07af2e2d22251bf9078a2d
Nicolas <angel.nicolas129@gmail.com>
2023-10-25 17:49:19 -0300

    Update vlasov_code.cpp

--- vlasov_code.cpp
+++ vlasov_code.cpp
@@ -11,18 +11,21 @@ double x_min = -2.0;
 double x_max = 2.0;
 double p_min = -2.0;
 double p_max = 2.0;
-const int n_x = 10;
-const int n_p = 10;
+const int n_x = 100;
+const int n_p = 100;
 double epsilon = 1.0;
 double d_t = 0.01;
 
 
 double x_values[n_x];
 double p_values[n_p];
-
 double force[n_x];
+double main_func[n_x][n_p];
+double func_one[n_x][n_p];
+double func_two[n_x][n_p];
 
-// da las condiciones de borde en negativo
+
+//do the function of % but with negative numbers
 int b_c(int value)
 {
     if (value < 0)
@@ -36,10 +39,14 @@ int b_c(int value)
 // ---------------------------- waterbag array-----------------------------------------------------
 void water_bag(double function[n_x][n_p])
 {
-    for (int j = 0; j < n_p; j++) {
-        for (int i = 0; i < n_x; i++) {
-            if (abs(x_values[i]) < x0) {
-                if (abs(p_values[j]) < p0) {
+    for (int j = 0; j < n_p; j++)
+    {
+        for (int i = 0; i < n_x; i++)
+        {
+            if (abs(x_values[i]) < x0)
+            {
+                if (abs(p_values[j]) < p0)
+                {
                     function[i][j] = 1.0 / (4.0 * x0 * p0);
                 } else {
                     function[i][j] = 0.0;
@@ -47,7 +54,6 @@ void water_bag(double function[n_x][n_p])
             }
         }
     } 
-
 }
 // ----------------------------Creation of x and p values array-----------------------------------------------------
 void init_xvalues()
@@ -64,8 +70,6 @@ void init_pvalues()
         p_values[i] = p_min + ((p_max - p_min) * i) / (n_p - 1);
     }
 }
-
-
 
 //--------------------------------FUNCIONES DEL CUBIC SPLINE-------------------------------------------------
 double Alpha(double x, int i)
@@ -87,162 +91,189 @@ double Delta(double x, int i)
     return ((pow(beta, 3) - beta) / 6) * pow((x_values[(i + 1) % n_x] - x_values[i]), 2);
 }
 
+//---------------------------------SECONDS DERIVATES----------------------------------------------------------
 
-//-------second derivate-------------------
-
-
-double sec_derivate_array(double func_array[n_x][n_p],int i,int j)
+//second derivative in x
+double sec_drv_x(double func_array[n_x][n_p],int i,int j)
 {
     return -(1/560)*func_array[b_c(i-4)][j] + (8/315)*func_array[b_c(i-3)][j] - (1/5)*func_array[b_c(i-2)][j] + (8/5)*func_array[b_c(i-1)][j] - (205/72)*func_array[(i)%n_x][j] + (8/5)*func_array[(i+1)%n_x][j] - (1/5)*func_array[(i+2)%n_x][j] + (8/315)*func_array[(i+3)%n_x][j] - (1/560)*func_array[(i+4)%n_x][j];
-
 }
-
-double sec_derivate_array_plus(double func_array[n_x][n_p],int i,int j)
+//second derivative in x with i+1
+double sec_drv_x_plus(double func_array[n_x][n_p],int i,int j)
 {
     return -(1/560)*func_array[b_c(i+1-4)][j] + (8/315)*func_array[b_c(i+1-3)][j] - (1/5)*func_array[b_c(i+1-2)][j] + (8/5)*func_array[i+1-1][j] - (205/72)*func_array[(i+1)%n_x][j] + (8/5)*func_array[(i+1+1)%n_x][j] - (1/5)*func_array[(i+1+2)%n_x][j] + (8/315)*func_array[(i+1+3)%n_x][j] - (1/560)*func_array[(i+1+4)%n_x][j];
 }
 
+//second derivative in p
+double sec_drv_p(double func_array[n_x][n_p],int i,int j)
+{
+    return -(1/560)*func_array[i][b_c(j-4)] + (8/315)*func_array[i][b_c(j-3)] - (1/5)*func_array[i][b_c(j-2)] + (8/5)*func_array[i][b_c(j-1)] - (205/72)*func_array[i][(j)%n_x] + (8/5)*func_array[i][(j+1)%n_x] - (1/5)*func_array[i][(j+2)%n_x] + (8/315)*func_array[i][(j+3)%n_x] - (1/560)*func_array[i][(j+4)%n_x];
+}
+//second derivative in p with j+1
+double sec_drv_p_plus(double func_array[n_x][n_p],int i,int j)
+{
+    return -(1/560)*func_array[i][b_c(j+1-4)] + (8/315)*func_array[i][b_c(j+1-3)] - (1/5)*func_array[i][b_c(j+1-2)] + (8/5)*func_array[i][j+1-1] - (205/72)*func_array[i][(j+1)%n_x] + (8/5)*func_array[i][(j+1+1)%n_x] - (1/5)*func_array[i][(j+1+2)%n_x] + (8/315)*func_array[i][(j+1+3)%n_x] - (1/560)*func_array[i][(j+1+4)%n_x];
+}
+
+
+
 //--------------------------------FUNCIONES DEL CUBIC SPLINE-------------------------------------------------
 
-double Func_csp_array_x(double func_array[n_x][n_p],double x,int i,int j)
+double func_csp_x(double func_array[n_x][n_p],double x,int i,int j)
 {
-    return Alpha(x,i)*func_array[i][j]+ Beta(x,i)*func_array[(i+1)%n_x][j]+ Gamma(x,i)*sec_derivate_array(func_array,i,j) + Delta(x,i)*sec_derivate_array_plus(func_array,i,j)   ;
+    return Alpha(x,i)*func_array[i][j]+ Beta(x,i)*func_array[(i+1)%n_x][j]+ Gamma(x,i)*sec_drv_x(func_array,i,j) + Delta(x,i)*sec_drv_x_plus(func_array,i,j)   ;
 }
-double Func_csp_array_p(double func_array[n_x][n_p],double p,int i,int j)
+double func_csp_p(double func_array[n_x][n_p],double p,int i,int j)
 {
-    return  Alpha(p,j)*func_array[i][j] + Beta(p,j)*func_array[i][(j+1)%n_p] + Gamma(p,j)*sec_derivate_array(func_array,i,j) + Delta(p,j)*sec_derivate_array_plus(func_array,i,j);
+    return  Alpha(p,j)*func_array[i][j] + Beta(p,j)*func_array[i][(j+1)%n_p] + Gamma(p,j)*sec_drv_p(func_array,i,j) + Delta(p,j)*sec_drv_p_plus(func_array,i,j);
 
 }
 
-
-//------------------------------FALTA LA FUNCION DE LA INTEGRAL----------------------------------------------
 //------------DEBE SER UNA INTEGRACION NUMERICA, CASO RAPIDO, TRAPECIOS, CASO LENTO, CUADRATURA--------------
 
+//trapesoid integral k=1 to n-1, in this case 1 to n_x-1.
+double sum_fxj(double fab[n_x][n_p],int x) 
+{
+    double sum = 0;
+    for (int j = 1; j < n_p-1; j++)
+    {
+        sum = sum + fab[x][j];
+    }
+    return sum;
+}
+//Is the rightest therms of the double trapesoid integral to the space integral with "b" variable x_values[k]
+double sum_f_sum_b(double fab[n_x][n_p],int k) // k do the function of n-1 in the loop
+{
+    double sum = 0;
+    for (int i = 1; i < k; i++)
+    {
+        for (int j = 1; j < n_p-1; j++)
+        {
+            sum = sum + (((fab[i][0]+fab[i][n_p-1])*(1/2)) + sum_fxj(fab,i));
+        }
+    }
+    return sum;
+}
+//Is the rightest therms of the double trapesoid integral to the space integral with "a" variable x_values[k]
+double sum_f_sum_a(double fab[n_x][n_p],int k) // k do the function of n-1 in the loop
+{
+    double sum = 0;
+    for (int i = k; i < n_x-1; i++)
+    {
+        for (int j = 1; j < n_p-1; j++)
+        {
+            sum = sum + (((fab[i][0]+fab[i][n_p-1])*(1/2)) + sum_fxj(fab,i));
+        }
+    }
+    return sum;
+}
 
-// double integration()
-// {
-//     double h = (x_max - x_min)/ n_x    
-//     for 
+//Double trapezoid integral and add result to force array
+void integration(double force[n_x],double f[n_x][n_p])
+{
+    for (int k=0 ; k < n_x ; k++)
+    {
+        double hx_b = (x_values[k]-x_min)/ (k+1);
+        double hx_a = (x_max-x_values[k])/ (n_x-k);
+        double hp = (p_max - p_min)/ n_p ;
 
-// }
-
-
-
-
-// def integration_array_eval(functions_array): # requiere igualar a una variable
-//     integral = np.zeros(n_x)#.astype('O')
-//     for k in range(n_x):
-//         for j in range(n_p):
-//             for i in range(n_x):
-//                 integral[k] = sp.integrate(-epsilon*functions_array[i][j].subs({x: x1, p: p1}), (x1, x_min,x_values[k]), (p1, p_min,p_max))+sp.integrate(-epsilon*-functions_array[i][j].subs({x: x1, p: p1}), (x1,x_values[k], x_max ), (p1,p_min, p_max))
-//     return integral
-
-
+        if ( k < 2)
+        {
+            force[k] = hp*(epsilon)*(hx_b*(-1)*((1/4)*(f[0][0]+f[0][n_p-1]+f[k][0]+f[k][n_p-1])) ) + (hx_a*( (1/4)*(f[k][0]+f[k][n_p-1] + f[n_x-1][k]+f[k][n_p-1]) ));
+        }
+        else
+        {
+            force[k] = (hx_b*hp*( ((1/4)*( f[0][0]+f[0][n_p-1] + 2*sum_fxj(f,0) + f[k][0]+f[k][n_p-1] + sum_fxj(f,k) )) +sum_f_sum_b(f,k)  )) + ( hx_a*hp*( (1/4)*((f[k][0]+f[k][n_p-1]+2*sum_fxj(f,k)) + (f[n_x-1][0]+f[n_x-1][n_p-1]+2*sum_fxj(f,n_x-1))) + sum_f_sum_a(f,k))) ;
+        }
+    }
+}
 
 //-----------------------------------FUNCIONES DEL LOOP--------------------------------------------------------
 
-void func_one(double func_array[n_x][n_p], double func_one[n_x][n_p])
+void advection_x(double func_array[n_x][n_p], double func_one[n_x][n_p])
 {
     for (int j = 0; j < n_p; j++) {
         for (int i = 0; i < n_x; i++) {
-            func_one[i][j] = Func_csp_array_x(func_array,x_values[i]-((p_values[j]*d_t)/2)    ,i,j);
+            func_one[i][j] = func_csp_x(func_array,x_values[i]-((p_values[j]*d_t)/2),i,j);
         }
     }
 }
-//- ((p_values[j] * d_t)/2)
-
-void func_two(double func_array[n_x][n_p], double func_one[n_x][n_p],double force[n_x])
+void advection_p(double func_array[n_x][n_p], double func_one[n_x][n_p],double force[n_x])
 {
     for (int j = 0; j < n_p; j++) {
         for (int i = 0; i < n_x; i++) {
-            func_one[i][j] = Func_csp_array_x(func_array,p_values[j] - (force[i] * d_t),i,j);
+            func_one[i][j] = func_csp_p(func_array,p_values[j] + (force[i] * d_t),i,j);
         }
+    }
+}
+
+//-----------------------------------
+
+void reset(double func[n_x])
+{
+    for (int i = 0; i < n_p; i++) {
+            func[i] = 0.0;
     }
 }
 
 
 
-double main_func[n_x][n_p];
-double func_one_array[n_x][n_p];
+//-----------------------------------MAIN--------------------------------------------------------
+
 
 int main()
 { 
 
-    init_xvalues();//inicia los valores de x
-    init_pvalues();//inicia los valores de p 
+    init_xvalues();//inicia los valores de x // SE RECOMIENDA CAMBIAR POR ALGUN CONSTRUCTOR Y NO INICIALIZAR
+    init_pvalues();//inicia los valores de p  
+    water_bag(main_func);// crea la funcion inicial
+        
+    // printf("main_func \n");
+    //  for (int i = 0; i < n_x; i++) {
+    //     for (int j = 0; j < n_p; j++) {
+    //         //cout << main_func[(i+1)%n_x][j];
+    //         printf("%f ", main_func[i][j]);
+    //     }
+    //     printf("\n");
+    // } 
 
-   
-    water_bag(main_func);// crea la funcion inicial 
-    func_one(main_func,func_one_array);// realiza el primer paso, first advetion in x.
+    cout<< main_func[n_x/2][n_p/2]<<endl;
+    int loop=0;
+    for(int t = 0; t < 40; t++)
+    {
+        advection_x(main_func,func_one);// realiza el primer paso, first advetion in x.
+        reset(force);// resetea el array force
+        integration(force,func_one);// realiza la integracion y la guarda en force
 
-    printf("main_func \n");
-     for (int i = 0; i < n_x; i++) {
-        for (int j = 0; j < n_p; j++) {
-            //cout << main_func[(i+1)%n_x][j];
-            printf("%f (%d %d) ", main_func[i][j],i,j);
-        }
-        printf("\n");
-    }   
+        advection_p(func_one,func_two,force);// realiza el segundo paso, second advetion in p.
 
-    printf("func_one_array \n");
-    for (int i = 0; i < n_x; i++) {
-        for (int j = 0; j < n_p; j++) {
-            //cout << main_func[(i+1)%n_x][j];
-            printf("%f (%d %d) ", func_one_array[i][j],i,j);
-        }
-        printf("\n");
+        advection_x(func_two,main_func);// realiza el tercer paso, third advetion in x.
+        cout<<"func_one"<<func_one[n_x/2][n_p/2]<<endl;
+        cout<<"func_two"<<func_two[n_x/2][n_p/2]<<endl;
+        cout<<"main_func"<<main_func[n_x/2][n_p/2]<<endl;
+        loop++;
+        cout<<"---------------loop "<<loop<<endl;
     }
+  
+
+
+
+    // printf("main after loop %d \n" ,loop);
+    //  for (int i = 0; i < n_x; i++) {
+    //     for (int j = 0; j < n_p; j++) {
+    //         //cout << main_func[(i+1)%n_x][j];
+    //         printf("%f ", main_func[i][j]);
+    //     }
+    //     printf("\n");
+    // }   
+    // cout<<"v_dxmaxima"<<(x_values[2]-x_values[1])/d_t <<endl;
+    // for (int i = 0; i < n_x; i++)
+    // {
+
+    //     cout<<force[i]<<endl;
+    // }
+    //cout<<force[4]<<endl;
 
     
-
+    return 0;
 }
-
-
-
-
-
-    // for (int j = 0; j < n_p; j++) {
-    //     for (int i = 0; i < n_x; i++) {
-    //         printf("%i%i ", i,j);
-    //     }
-    //     printf("\n");
-    // }    
-
-    // //printf("%f", function[4][4]);    
-    // // for (int j = 0; j < n_p; j++) {
-    // //     for (int i = 0; i < n_x; i++) {
-    // //         printf("%f ",  sec_derivate_array_plus(function,i,j));
-    // //     }
-    // //     printf("\n");
-    // // } 
-
-
-        // int func_array[n_x][n_p];
-    // for (int i = 0; i < n_x; i++) {
-    //     for (int j = 0; j < n_p; j++) {
-    //         func_array[i][j] = i * 10 + j;
-    //     }
-    // }
-    // printf("%d", func_array[2][1]);
-//  //   Imprimir la matriz func_array en forma de matriz
-    // for (int i = 0; i < n_x; i++) {
-    //     for (int j = 0; j < n_p; j++) {
-    //         printf("%d ", func_array[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-
-
-
-
-
-    // int func_array[n_x][n_p];
-    // for (int i = 0; i < n_x; i++) {
-    //     for (int j = 0; j < n_p; j++) {
-    //         func_array[i][j] = i * 10 + j;
-    //     }
-    // }
-
-    // cout << b_c(1-3) << endl;
-    // printf("%d", func_array[-3%n_x][1]);
